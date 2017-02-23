9836cfb4ed33c0a81e53838f23ad997da7b47455
Stian Westerhus Vale <stvale@Stians-MacBook-Pro-2.local>
2017-02-22 20:24:34 -0800

    instant update

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,13 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(req, res){
-  	res.render('index', projects);
   	projects["grid"] = false;
+  	res.render('index', projects);
+  	
 };
 
 exports.viewGrid = function(req,res){
+	projects["grid"] = true;
 	res.render('index', projects);
-  	projects["grid"] = true;	
-
+  	
 }
